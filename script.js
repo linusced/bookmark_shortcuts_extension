59d08d3598e0c604633fa0e243ddf97c20cec066
= <=>
2022-10-05 09:34:24 +0200

    1.2.0 bookmarks with folders bug fix

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-var bookmarks = []
+var bookmarks = [];
+
+update();
 function update() {
-    chrome.bookmarks.getRecent(100, result => {
+    chrome.bookmarks.getRecent(500, result => {
         if (chrome.runtime.lastError) {
-            console.warn(chrome.runtime.lastError)
-            return update()
+            console.error(chrome.runtime.lastError);
+            return;
         }
-        result.sort((a, b) => a.index - b.index)
-        if (result.length > 10) result = result.slice(0, 10)
-        bookmarks = []
-        result.forEach(item => bookmarks.push({ command: `bookmark_${item.index}`, url: item.url }))
-    })
-}
-update()
-chrome.commands.onCommand.addListener(command => bookmarks.forEach(item => {
-    if (item.command === command) chrome.tabs.getAllInWindow(null, tabs => {
-        let tabExists = false
-        tabs.forEach(t => {
-            if (t.url === item.url) {
-                chrome.tabs.update(t.id, { active: true })
-                tabExists = true
+
+        bookmarks = [];
+        result.sort((a, b) => a.index - b.index);
+
+        for (let i = 0; i < result.length; i++)
+            if (result[i].parentId === "1") {
+                bookmarks.push({ command: `bookmark_${result[i].index}`, url: result[i].url });
+
+                if (bookmarks.length >= 10)
+                    break;
             }
-        })
-        if (!tabExists) chrome.tabs.query({ active: true }, t => t[0].url === 'chrome://newtab/' ? chrome.tabs.update(t[0].id, { url: item.url }) : chrome.tabs.create({ url: item.url }))
-    })
-}))
-chrome.bookmarks.onCreated.addListener(update)
-chrome.bookmarks.onRemoved.addListener(update)
-chrome.bookmarks.onChanged.addListener(update)
-chrome.bookmarks.onMoved.addListener(update)
-chrome.runtime.onInstalled.addListener(installed => installed.reason === 'install' ? chrome.tabs.create({ url: 'chrome://extensions/shortcuts' }) : null)
+    });
+}
+
+chrome.commands.onCommand.addListener(command => {
+    for (let i = 0; i < bookmarks.length; i++)
+        if (bookmarks[i].command === command) {
+            chrome.tabs.getAllInWindow(null, tabs => {
+                let tabExists = false;
+                for (let t = 0; t < tabs.length; t++)
+                    if (tabs[t].url === bookmarks[i].url) {
+                        chrome.tabs.update(tabs[t].id, { active: true });
+                        tabExists = true;
+                        break;
+                    }
+
+                if (!tabExists)
+                    chrome.tabs.query({ active: true }, t => t[0].url === 'chrome://newtab/' ? chrome.tabs.update(t[0].id, { url: bookmarks[i].url }) : chrome.tabs.create({ url: bookmarks[i].url }));
+            });
+            break;
+        }
+});
+
+chrome.bookmarks.onCreated.addListener(update);
+chrome.bookmarks.onRemoved.addListener(update);
+chrome.bookmarks.onChanged.addListener(update);
+chrome.bookmarks.onMoved.addListener(update);
+chrome.runtime.onInstalled.addListener(installed => {
+    if (installed.reason === 'install')
+        chrome.tabs.create({ url: 'chrome://extensions/shortcuts' });
+});
